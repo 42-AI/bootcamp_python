31c3458b4f59fbe5b865b3818aeab083c2c1aaab
Augustin LOPEZ <aulopez@e1r5p17.42.fr>
2021-12-09 11:48:52 +0100

    ex00 and script

--- module00/ex00/installation.sh
+++ module00/ex00/installation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function which_dl {
+    # If operating system name contains Darwnin: MacOS. Else Linux
     if uname -s | grep -iqF Darwin; then
         echo "Miniconda3-latest-MacOSX-x86_64.sh"
     else
@@ -9,6 +10,7 @@ function which_dl {
 }
 
 function which_shell {
+    # if $SHELL contains zsh, zsh. Else Bash
     if echo $SHELL | grep -iqF zsh; then
         echo "zsh"
     else
@@ -16,10 +18,22 @@ function which_shell {
     fi
 }
 
+function when_conda_exist {
+    # check and install 42AI environement
+    printf "Checking 42AI environment: "
+    if conda info --envs | grep -iqF 42AI; then
+        printf "\e[33mDONE\e[0m\n"
+    else
+        printf "\e[31mKO\e[0m\n"
+        printf "\e[33mCreating 42AI environnment:\e[0m\n"
+        conda update -n base -c defaults conda -y
+        conda create --name 42AI python=3.7 jupyter numpy pandas pycodestyle -y
+    fi
+}
+
 function set_conda {
-    HOME=$(echo ~)
-    INSTALL_PATH="/goinfre"
-    MINICONDA_PATH=$INSTALL_PATH"/miniconda3/bin"
+    MINICONDA_PATH="/goinfre/$USER/miniconda3"
+    CONDA=$MINICONDA_PATH"/bin/conda"
     PYTHON_PATH=$(which python)
     REQUIREMENTS="jupyter numpy pandas pycodestyle"
     SCRIPT=$(which_dl)
@@ -27,27 +41,35 @@ function set_conda {
     DL_LINK="https://repo.anaconda.com/miniconda/"$SCRIPT
     DL_LOCATION="/tmp/"
 
-
-    if $MINICONDA_PATH/conda info --envs | grep -iqF 42AI; then
-	    echo "nothing to be done :)"
+    printf "Checking conda: "
+    TEST=$(conda -h 2>/dev/null)
+    if [ $? == 0 ] ; then
+        printf "\e[32mOK\e[0m\n"
+        when_conda_exist
         return
     fi
-    if [ ! -d $MINICONDA_PATH ]; then
-        if [ ! -f $DL_LOCATION$SCRIPT ]; then
-            cd $DL_LOCATION
-		    curl -LO $DL_LINK
-            cd -
-        fi
-	    sh $DL_LOCATION$SCRIPT -b -p $INSTALL_PATH"/miniconda3" || true
-	fi
-    $MINICONDA_PATH/conda init $MY_SHELL
-    $MINICONDA_PATH/conda config --set auto_activate_base false
+    printf "\e[31mKO\e[0m\n"
+    if [ ! -f $DL_LOCATION$SCRIPT ]; then
+        printf "\e[33mDonwloading installer:\e[0m\n"
+        cd $DL_LOCATION
+		curl -LO $DL_LINK
+        cd -
+    fi
+    printf "\e[33mInstalling conda:\e[0m\n"
+	sh $DL_LOCATION$SCRIPT -b -p $MINICONDA_PATH
+    printf "\e[33mConda initial setup:\e[0m\n"
+    $CONDA init $MY_SHELL
+    $CONDA config --set auto_activate_base false
+
+    printf "\e[33mRelaunching shell:\e[0m\n"
     if [ $MY_SHELL == "zsh" ]; then
         source ~/.zshrc
     else
         source ~/.bash_profile
     fi
-    conda create --name 42AI python=3.7 jupyter numpy pandas pycodestyle
+
+    printf "\e[33mCreating 42AI environnment:\e[0m\n"
+    $CONDA create --name 42AI python=3.7 jupyter numpy pandas pycodestyle -y
 }
 
 set_conda
